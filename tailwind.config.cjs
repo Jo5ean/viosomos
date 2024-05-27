b1fcdd13e7a485277e17f7c780c40fe2ae14deb0
Jo5ean <josean.urbani@gmail.com>
2024-05-27 01:19:58 -0300

    Casi terminado!

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+            './node_modules/flowbite/**/*.js'
+           ],
   presets: [],
   darkMode: 'media', // or 'class'
   theme: {
@@ -964,5 +966,5 @@ module.exports = {
     'active',
     'disabled',
   ],
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
